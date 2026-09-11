334ca59d2b8ce685f40d8224a6fb7a1c2f84091d
Clinopyroxene <greenpyroxene@gmail.com>
2026-09-11 14:00:09 -0400

    Tried to make a spawner with prefabs and lists for debug lines

--- Assets/Scripts/SquareSpawner.cs
+++ Assets/Scripts/SquareSpawner.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SquareSpawner : MonoBehaviour
 {
     public GameObject debugPrefab;
-
+    public bool spawn;
+    public List<GameObject> Squares;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,12 +17,39 @@ public class SquareSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         float mousePosX = mousePos.x;
         float mousePosY = mousePos.y;
-        if (Mouse.current.leftButton.isPressed)
+        transform.position = mousePos;
+        if (Mouse.current.leftButton.wasPressedThisFrame)
         {
+            spawn = true;
+            //Vector2 line1 = new Vector2(mousePosX, mousePosY + 1);
+            //Vector2 line2 = new Vector2(mousePosX + 1, mousePosY);
+            //Vector2 line3 = new Vector2(mousePosX + 1, mousePosY + 1);
+            //Vector2 line4 = new Vector2(mousePosX + 1, mousePosY + 1);
+            //Debug.DrawLine(mousePos, line1);
+            //Debug.DrawLine(mousePos, line2);
+            //Debug.DrawLine(line1, line3);
+            //Debug.DrawLine(line2, line4);
+            
+        }
+        if(spawn == true)
+        {
+            spawn = false;
             Instantiate(debugPrefab);
+            //Vector2 line1 = new Vector2(mousePosX, mousePosY + 1);
+            //Vector2 line2 = new Vector2(mousePosX + 1, mousePosY);
+            //Vector2 line3 = new Vector2(mousePosX + 1, mousePosY + 1);
+            //Vector2 line4 = new Vector2(mousePosX + 1, mousePosY + 1);
+            //Debug.DrawLine(mousePos, line1);
+            //Debug.DrawLine(mousePos, line2);
+            //Debug.DrawLine(line1, line3);
+            //Debug.DrawLine(line2, line4);
+        }
+        for(int i = 0; i < Squares.Count; i++)
+        {
             Vector2 line1 = new Vector2(mousePosX, mousePosY + 1);
             Vector2 line2 = new Vector2(mousePosX + 1, mousePosY);
             Vector2 line3 = new Vector2(mousePosX + 1, mousePosY + 1);
@@ -29,7 +58,6 @@ public class SquareSpawner : MonoBehaviour
             Debug.DrawLine(mousePos, line2);
             Debug.DrawLine(line1, line3);
             Debug.DrawLine(line2, line4);
-            
         }
     }
 }
