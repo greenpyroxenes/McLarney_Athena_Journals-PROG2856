d752b260915a1e6350400d78fa97ccebf567c186
Clinopyroxene <greenpyroxene@gmail.com>
2026-09-15 13:46:20 -0400

    Created bomb trail

--- Assets/Journal 2/Scripts/Controllers/Player.cs
+++ Assets/Journal 2/Scripts/Controllers/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
     public Vector3 bombOffset = new Vector3(0f, 1f, 0f);
+    public float bombTrailSpacing;
+    public int numberOfBombs;
 
     // Update is called once per frame
     void Update()
@@ -16,10 +18,24 @@ public class Player : MonoBehaviour
         {
             SpawnBombAtOffset(bombOffset);
         }
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfBombs);
+        }
     }
 
     void SpawnBombAtOffset(Vector3 inOffset)
     {
         Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+    }
+
+    void SpawnBombTrail(float bombSpacing, int numberOfBombs)
+    {
+        Vector2 bombPos = transform.position;
+        for (int i = 0; i < numberOfBombs; i++)
+        {
+            Instantiate(bombPrefab, bombPos, Quaternion.identity);
+            bombPos.y -= bombSpacing;
+        }
     }
 }
