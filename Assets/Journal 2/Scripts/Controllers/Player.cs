db3a8157ad19d796f64be38881c1e086c52d9098
Clinopyroxene <greenpyroxene@gmail.com>
2026-09-15 12:57:38 -0400

    Took code from class to have bomb spawn

--- Assets/Journal 2/Scripts/Controllers/Player.cs
+++ Assets/Journal 2/Scripts/Controllers/Player.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-    
+    public Vector3 bombOffset = new Vector3(0f, 1f, 0f);
+
     // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            SpawnBombAtOffset(bombOffset);
+        }
+    }
 
+    void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
     }
 }
