92b65989d0297ddb109b5a22ac058b1a97f4ded0
Clinopyroxene <greenpyroxene@gmail.com>
2026-09-26 00:57:03 -0400

    started asteriod movement

--- Assets/Space Shooter Journals/Scripts/Controllers/Asteroid.cs
+++ Assets/Space Shooter Journals/Scripts/Controllers/Asteroid.cs
@@ -11,10 +11,12 @@ public class Asteroid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 }
