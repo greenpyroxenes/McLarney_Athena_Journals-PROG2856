2e05bb9b205e792b409b370180b4d8db5ca323f1
Clinopyroxene <greenpyroxene@gmail.com>
2026-09-25 22:23:52 -0400

    added enemy movement

--- Assets/Space Shooter Journals/Scripts/Controllers/Enemy.cs
+++ Assets/Space Shooter Journals/Scripts/Controllers/Enemy.cs
@@ -1,12 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Enemy : MonoBehaviour
 {
-    
-    private void Update()
+    public Player playerScript;
+    public Transform playerPos;
+    public float speed = 1f;
+    float acceleration;
+    public float accelerationTime = 1f;
+    public Vector3 velocity;
+
+    void Start()
     {
-        
+        acceleration = speed / accelerationTime;
     }
 
+    private void Update()
+    { 
+        ShipMovement(playerPos);
+    }
+
+    void ShipMovement(Transform inPlayerPos)
+    {
+        inPlayerPos = playerScript.transform;
+
+        Vector3 direction = inPlayerPos.position - transform.position;
+        direction = direction.normalized;
+
+        velocity += acceleration * Time.deltaTime * direction;
+        velocity = Vector3.ClampMagnitude(velocity, speed);
+        transform.position += velocity * Time.deltaTime;
+    }
 }
